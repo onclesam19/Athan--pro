a098988ce1bf7d71d57b15d20f065ea6fea33eea
onclesam19 <samivitija81@gmail.com>
2026-07-20 00:35:30 +0200

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AthanApp from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AthanApp />
+  </React.StrictMode>,
+)
